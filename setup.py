5fa82c23f4f328b9a94c30487e6758a9ba0f2c28
BECKER-HICKL\enzo <marscheck@becker-hickl.de>
2023-01-03 14:07:41 +0100

    version bump to 1.0.1

--- setup.py
+++ setup.py
@@ -2,15 +2,18 @@ from distutils.core import setup
 setup(
   name = 'bhpy',
   packages = ['bhpy'],
-  version = '1.0.0',
+  version = '1.0.1',
   license='MIT License',
   description = "Python bindings to use Becker & Hickls' hardware control dll and API",
   author = 'Marscheck',
   author_email = 'marscheck@becker-hickl.de',
   url = 'https://www.becker-hickl.com/',
-  download_url = 'https://github.com/bhmarscheck/bhpy/archive/refs/tags/v1.0.0.tar.gz',
+  download_url = 'https://github.com/bhmarscheck/bhpy/archive/refs/tags/v1.0.1.tar.gz',
   keywords = ['FLIM', 'Fluorescence lifetime', 'TCSPC', 'photon counting', 'BH', 'Becker&Hickl', 'molecular imaging', 'SPC', 'SPCM'],
-  install_requires=['numpy>=1.23.0,<2'],
+  install_requires=[
+    'numpy>=1.23.5,<2',
+    'appdirs>=1.4.4,<2'
+  ],
   classifiers=[
     'Development Status :: 5 - Production/Stable', # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Science/Research',
