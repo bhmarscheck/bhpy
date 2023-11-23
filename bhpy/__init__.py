3482a8526f0cccc7589a3adb4c9275df323650d2
Enzo Marscheck <marscheck@becker-hickl.de>
2023-11-23 17:55:39 +0100

    first bulg of spcconnect wrapping code

--- bhpy/__init__.py
+++ bhpy/__init__.py
@@ -1,3 +1,4 @@
 from bhpy.spc_qc_wrapper import SpcQcDllWrapper, ModuleInit
 from bhpy.spc_qc_config import SpcQc104Conf
 from bhpy.bh_device_scan_wrapper import BHDeviceScanWrapper
+from bhpy.bh_remote_control import BHRemoteControl
